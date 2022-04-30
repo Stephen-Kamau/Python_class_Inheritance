5bf816baaa9955236841b2f5b35e6ac2c2a052a3
Stephen-Kamau <43881878+Stephen-Kamau@users.noreply.github.com>
2022-04-30 15:23:59 +0300

    Add files via upload
    Module to run already instantiated objects

---
+++ Q2.py
@@ -0,0 +1,7 @@
+from emp import FullTimeEmployee , Department , PartTimeEmployee , Company
+from emp import LeaveApplicationException , Leave , VaccinationLeave , Manager
+from objects import company , dpt1 , dpt2
+
+
+if __name__ == '__main__':
+    company.__str__()
